7b9cb190940f9ccdf4af3d071cc3e8e13c20ff2f
przemekdab1993 <przemekdab1993@gmail.com>
2018-07-02 20:59:08 +0200

    v 0.0.3

---
+++ script/funkcje.js
@@ -0,0 +1,86 @@
+var lista_kart =  ['tr', 're', 'rr', 'gf', 'sf', '4t', 'hy', 'fh', 'gt'];
+var wylosowane = [];
+var $dane_wy;	
+var $cursor = $('#game');
+var $klik;
+var licznik_odkrytych = 0;
+var $karta_1, $karta_2;
+
+function losuj_karty()
+{
+	for (var i = 0; i < 16; i++)
+	{
+		var los = Math.floor(Math.random() * 9);
+		wylosowane[i] = lista_kart[los];
+	}
+}
+function wypisz_wymaluj()
+{
+	for (var i = 0; i < wylosowane.length; i++)
+	{
+		var buf = '<div id="' + wylosowane[i] + '" class="cards_0">' + wylosowane[i] + '</div>';
+		$cursor.append(buf);
+	}
+}
+function reset_pary()
+{
+	$karta_1.addClass('cards_0');
+	$karta_1.removeClass('cards_1');
+	$karta_1.on('click', function(e) { odkryj(e); } );
+	$karta_2.addClass('cards_0');
+	$karta_2.removeClass('cards_1');
+	$karta_2.on('click', function(e) { odkryj(e); } );
+	licznik_odkrytych = 0;
+	$karta_1 = '';
+	$karta_2 = '';
+}
+function ukrycie_pary()
+{
+	$karta_1.animate({opacity: 0.0}, 800, function() { $karta_1.unbind('click'); });
+	$karta_2.animate({opacity: 0.0}, 800, function() { $karta_2.unbind('click'); });
+	$karta_1 = '';
+	$karta_2 = '';
+	licznik_odkrytych = 0;
+}
+function odkryj(e)
+{
+	var $target = $(e.target);
+	switch(licznik_odkrytych)
+	{
+		case 0:
+			
+			licznik_odkrytych++;
+			$target.addClass('cards_1');
+			$target.removeClass('cards_0');
+			$karta_1 = $target;
+			$karta_1.unbind('click');
+			break;
+		case 1:
+			licznik_odkrytych++;
+			$target.addClass('cards_1');
+			$target.removeClass('cards_0');
+			$karta_2 = $target;
+			$karta_2.unbind('click');
+			
+			if ($karta_1.html() === $karta_2.html())
+			{
+				ukrycie_pary();
+			}
+			else
+			{
+				setTimeout(function() { reset_pary(); },1500);
+			}
+			break;
+		default:
+			reset_pary();
+			break;
+	}
+}
+
+$( function() 
+{
+	losuj_karty();
+	wypisz_wymaluj();
+	$klik = $('.cards_0');
+	$klik.on('click', function(e) { odkryj(e); } );
+});
